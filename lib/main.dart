94cc5bcbfae52355e7caea9c812adf2890427e74
Prieto-Mayca-A <68166806+Prieto-Mayca-A@users.noreply.github.com>
2021-06-03 21:52:25 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,419 @@
+import 'package:codelabs/screen/basic_design.dart';
+import 'package:flutter/material.dart';
+
+ void main() => runApp(MyApp());
+  
+ class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FavoriteWidget(),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/fondo.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// #docregion FavoriteWidget
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+// #enddocregion FavoriteWidget
+
+// #docregion _FavoriteWidgetState, _FavoriteWidgetState-fields, _FavoriteWidgetState-build
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  // #enddocregion _FavoriteWidgetState-build
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  // #enddocregion _FavoriteWidgetState-fields
+
+  // #docregion _toggleFavorite
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+  // #enddocregion _toggleFavorite
+
+  // #docregion _FavoriteWidgetState-build
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+  // #docregion _FavoriteWidgetState-fields
+}
+/*
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filas y columnas'),
+      ),
+      body: Column(
+        children: [
+          Description('Inicio'),
+          Blocks(MainAxisAlignment.center, CrossAxisAlignment.start),
+          Description('Fin'),
+          Blocks(MainAxisAlignment.center, CrossAxisAlignment.end),
+          Description('Centro'),
+          Blocks(MainAxisAlignment.center, CrossAxisAlignment.center),
+        ],
+      ),
+    );
+  }
+}
+
+class Description extends StatelessWidget {
+  final String data;
+
+  const Description(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15),
+      child: Text(this.data, style: TextStyle(fontSize: 20)),
+    );
+  }
+}
+
+class Blocks extends StatelessWidget {
+  final MainAxisAlignment mainAlignment;
+  final CrossAxisAlignment crossAlignment;
+
+  const Blocks(this.mainAlignment, this.crossAlignment);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: this.mainAlignment,
+      crossAxisAlignment: this.crossAlignment,
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          color: Colors.tealAccent,
+          child: Text('Grande', style: TextStyle(fontSize: 20)),
+        ),
+        Container(
+          padding: EdgeInsets.all(5),
+          color: Colors.blueGrey,
+          child: Text('Pequeño', style: TextStyle(fontSize: 20)),
+        )
+      ],
+    );
+  }
+}
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  List lista = [
+    "https://s1.1zoom.me/big3/471/Painting_Art_Back_view_Photographer_575380_3840x2400.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_mYWz0iw-ERogMbOxUQUwpAO3V7DFarajmilJpNmtJlgVQeICeuEAp2Ilhda4IxJ4FWU&usqp=CAU",
+    
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Displaying Images"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext ctx, int index) {
+          return Padding(
+            padding: EdgeInsets.all(20),
+            child: Card(
+              shape:Border.all(width: 5, ),
+              elevation: 20,
+              color: Colors.black,
+              child: Column(
+                children: <Widget>[
+                  Image.network(lista[index]),
+                  SizedBox(height: 10,),  
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.yellow,
+                    size: 40,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: lista.length,
+      ),
+    );
+  }
+}
+*/
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),                         // ... to here.
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  
+    final _suggestions = <WordPair>[];
+    final _saved = <WordPair>{};     // NEW
+    final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      // The itemBuilder callback is called once per suggested
+      // word pairing, and places each suggestion into a ListTile
+      // row. For even rows, the function adds a ListTile row for
+      // the word pairing. For odd rows, the function adds a
+      // Divider widget to visually separate the entries. Note that
+      // the divider may be difficult to see on smaller devices.
+      itemBuilder: (BuildContext _context, int i) {
+        // Add a one-pixel-high divider widget before each row
+        // in the ListView.
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        // The syntax "i ~/ 2" divides i by 2 and returns an
+        // integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This calculates the actual number of word pairings
+        // in the ListView,minus the divider widgets.
+        final int index = i ~/ 2;
+        // If you've reached the end of the available word
+        // pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the
+          // suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+    Widget _buildRow(WordPair pair) {
+     final alreadySaved = _saved.contains(pair);
+  return ListTile(
+    title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ),
+    trailing: Icon(
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+    ),
+    onTap: () {      // NEW lines from here...
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else {
+          _saved.add(pair);
+        }
+      });
+    },               // ... to here.
+  );
+}
+
+void _pushSaved() {
+  Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
+    );
+  }
+
+}
+*/
+*/
